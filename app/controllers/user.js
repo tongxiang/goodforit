6eea9163b5a3b99f783ec0b270bc5d6c433bc28d
Ji Ho Koo <jihokoo@gmail.com>
2014-09-20 21:57:21 -0400

    enable linking with venmo

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,6 @@
 var User = require('../models/user')['User'];
 var request = require('request');
+var async = require('async');
 
 function getOne (req, res){
   if(req.user){
@@ -12,19 +13,57 @@ function getOne (req, res){
 };
 
 function getExpenses (user, cb){
-  var options = {
-    url: 'https://secure.splitwise.com/api/v3.0/get_expenses',
-    oauth: {
-      consumer_key: process.env.splitWiseConsumerKey,
-      consumer_secret: process.env.splitWiseConsumerSecret,
-      token: user.splitWiseProfile.token,
-      token_secret: user.splitWiseProfile.tokenSecret
-    },
-    json: true
-  }; 
-  request(options, function(a, b, c){
+  var splitWiseOptions;
+  var venmoOptions;
+  var parallelFunctions = [];
+  if(user.splitwiseId){
+    splitWiseOptions = {
+      url: 'https://secure.splitwise.com/api/v3.0/get_expenses',
+      oauth: {
+        consumer_key: process.env.splitWiseConsumerKey,
+        consumer_secret: process.env.splitWiseConsumerSecret,
+        token: user.splitWiseProfile.token,
+        token_secret: user.splitWiseProfile.tokenSecret
+      },
+      json: true
+    };
+    parallelFunctions.push(
+      function(callback){
+        request(splitWiseOptions, function(err, httpResponse, response){
+          callback(null, { splitwise: response.expenses });
+        });
+      }
+    );
+  }
+  if(user.venmoId){
+    venmoOptions = {
+      url: 'https://api.venmo.com/v1/payments',
+      qs: {
+        access_token: user.venmoProfile.accessToken,
+        limit: 50
+      },
+      json: true
+    };
+    parallelFunctions.push(
+      function(callback){
+        request(venmoOptions, function(err, httpResponse, response){
+          callback(null, { venmo: response.data });
+        })
+      }
+    );
+  }
+
+  async.parallel(
+    parallelFunctions, 
+  function(err, results){
     User.findOne({_id: user.id}, function(err, user){
-      user.splitWiseHistory = c.expenses;
+      results.forEach(function(element){
+        if(element.splitwise){
+          user.splitWiseHistory = element.splitwise;
+        } else if(element.venmo){
+          user.venmoHistory = element.venmo;
+        }
+      });
       user.save(function(err, user){
         cb(err, user);
       });
