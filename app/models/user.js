9906200e5ce2b18482ac02380822a976b54e266c
Tong Xiang <tong.xiang2@gmail.com>
2014-09-20 22:08:05 -0400

    unfinished connectednessDonutChart implementation, d3, nvd3 scripts added

--- app/models/user.js
+++ app/models/user.js
@@ -18,7 +18,11 @@ db.once('open', function callback () {
 var userSchema = new Schema({
   splitWiseId: String,
   splitWiseProfile: {},
-  splitWiseHistory: [{}]
+  splitWiseHistory: [{}],
+  activity: {}, // Cash outflow per unit of time.
+  reliability: {}, // However quickly rates have been How quickly debts have been resolved * length of credit history. 
+  responsibility: 0, // Decimal of 1. Percentage of debts repaid. 
+  connectedness: 0 // The number of parties the user transacts with, added to by each API call.
 });
 
 var User = mongoose.model('User', userSchema);
